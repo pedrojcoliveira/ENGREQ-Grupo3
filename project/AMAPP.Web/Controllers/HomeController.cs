6dcccb023a05f7cd510788c33072af07edcae702
pauloabreu13 <aubreupaulo130499@gmail.com>
2024-11-25 00:04:19 +0000

    Fix HomeController and Index

--- project/AMAPP.Web/Controllers/HomeController.cs
+++ project/AMAPP.Web/Controllers/HomeController.cs
@@ -1,35 +1,23 @@
 using AMAPP.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-
 namespace AMAPP.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
-            var products = new List<Product>
-        {
-            new Product { Id = 1, Name = "Product A", Price = 10.99m, Quantity = 5 },
-            new Product { Id = 2, Name = "Product B", Price = 20.49m, Quantity = 3 },
-            new Product { Id = 3, Name = "Product C", Price = 15.75m, Quantity = 8 }
-        };
-
-            return View(products);
+            return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
