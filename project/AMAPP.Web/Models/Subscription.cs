1eb36362493b3d65adf0dfdd5d220ae5da96e292
pauloabreu13 <aubreupaulo130499@gmail.com>
2024-12-08 23:46:18 +0000

    Frontend Create Subscriptions

--- project/AMAPP.Web/Models/Subscription.cs
+++ project/AMAPP.Web/Models/Subscription.cs
@@ -8,24 +8,6 @@
         public int ProductOfferId { get; set; }
         public int SubscriptionId { get; set; }
         public int Quantity { get; set; }
-        public List<SubscriptionPaymentViewModel> Payments { get; set; }
 
-        // Propriedades para Criação
-        public List<CreateSubscriptionPaymentViewModel> CreateSubscriptionPayments { get; set; }
-    }
-
-    public class SubscriptionPaymentViewModel
-    {
-        public int SelectedProductOfferId { get; set; }
-        public DateTime PaymentDate { get; set; }
-        public decimal Amount { get; set; }
-        public int PaymentStatus { get; set; }
-    }
-
-    public class CreateSubscriptionPaymentViewModel
-    {
-        public int SelectedProductOfferId { get; set; }
-        public DateTime PaymentDate { get; set; }
-        public decimal Amount { get; set; }
     }
 }
