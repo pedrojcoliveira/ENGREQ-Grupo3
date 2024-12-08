0aa56820c5bc1385f1600c1e0f6dd9bb832364a9
hugocoelho-isep <1162086@isep.ipp.pt>
2024-12-08 12:30:15 +0000

    Add Kpis controller and service

---
+++ project/AMAPP.API/Services/Implementations/KpiService.cs
@@ -0,0 +1,31 @@
+﻿using AMAPP.API.DTOs.Kpi;
+using AMAPP.API.Repository.KpiRepository;
+using AMAPP.API.Services.Interfaces;
+
+namespace AMAPP.API.Services.Implementations
+{
+    public class KpiService : IKpiService
+    {
+
+        private readonly IKpiRepository _kpiRepository;
+
+        public KpiService(IKpiRepository kpiRepository)
+        {
+            _kpiRepository = kpiRepository;
+        }
+
+        public async Task<IEnumerable<CoproducerSubscriptionKpi>> GetCoproducerSubscriptionKpiAsync()
+        {
+            var coproducerSubscriptionKpi = await _kpiRepository.GetCoproducerSubscriptionKpiAsync();
+
+            return coproducerSubscriptionKpi;
+        }
+
+        public async Task<IEnumerable<ProducerDeliveryKpi>> GetProducerDeliveryKpiAsync()
+        {
+            var producerDeliveryKpi = await _kpiRepository.GetProducerDeliveryKpiAsync();
+
+            return producerDeliveryKpi;
+        }
+    }
+}
