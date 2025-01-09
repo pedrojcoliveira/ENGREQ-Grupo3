e691da9147adc463df88d915eb8fbafcc81372dd
pauloabreu13 <aubreupaulo130499@gmail.com>
2025-01-09 20:20:20 +0000

    Fix Create and Edit Products

--- project/AMAPP.Web/Constants.cs
+++ project/AMAPP.Web/Constants.cs
@@ -4,4 +4,12 @@ public static class Constants
 {
     public enum SubscriptionDuration    {  Semanal,        Semestral,        Anual    }
     public enum ResourceStatus    {  Ativo,     Inativo}
+
+    public enum DeliveryUnit
+    {
+        Unidade,
+        Kg,
+        Gramas,
+        Litros
+    }
 }
